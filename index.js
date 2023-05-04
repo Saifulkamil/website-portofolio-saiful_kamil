07a4421f711f77b8223667e10e4d353f95924e5e
Saiful kamil <109580466+sepol-sys@users.noreply.github.com>
2023-05-04 20:18:00 +0700

    deploy app engine

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.get('/name', (req, res) => {
-  res.send('saiful kamil')
-})
+// app.get('/name', (req, res) => {
+//   res.send('saiful kamil')
+// })
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ public/script.js
@@ -0,0 +1,11 @@
+// const warp = document.querySelector("main .content #skill .cover .warp");
+
+// const dregging = (e) =>{
+//     // scroll.scrollLeft = e.pagex;
+//     console.log(e.pageX);
+// }
+
+// warp.addEventListener("move", dregging)
+
+
+// const showMore = document.querySelector('.showMore')
